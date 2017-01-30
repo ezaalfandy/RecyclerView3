8694a6e34881e74e44e4e61eb72b003f78ca0b01
Alfandy <ezaalfandy.ea@gmail.com>
2017-01-30 19:56:34 +0700

    RecyclerVIew VIew Add

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -1,5 +1,7 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3;
 
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
@@ -19,8 +21,10 @@ import java.util.ArrayList;
 import id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter;
 import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements HotelAdapter,HotelAdapter.IHotelAdapter{
 
+    private static final String HOTEL = "hotel";
+    private static final int REQUEST_CODE_ADD = 88;
     ArrayList<Hotel> mList= new ArrayList<>();
     HotelAdapter mAdapter;
     @Override
@@ -29,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerVIew);
         LinearLayoutManager layoutManager= new LinearLayoutManager (this);
         recyclerView.setLayoutManager(layoutManager);
-        mAdapter = new HotelAdapter(mList);
+        mAdapter = new HotelAdapter(this.mList);
         recyclerView.setAdapter(mAdapter);
 
         fillData();
@@ -41,23 +45,53 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view){
+                goAdd();
+            }
+        });
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                goAdd();
             }
+
+
         });
     }
+
+    private void goAdd() {
+        startActivityForResult(new Intent(this,InputActivity.class), REQUEST_CODE_ADD);
+
+    }
+
     private void fillData() {
         Resources resources = getResources();
         String [] arJudul = resources.getStringArray(R.array.places);
         String [] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        String [] arDetail = resources.getStringArray(R.array.place_details);
+        String [] arLokasi = resources.getStringArray(R.array.place_Locations);
         TypedArray a = resources.obtainTypedArray(R.array.places_picture);
         Drawable[] arFoto  = new Drawable[a.length()];
         for (int i = 0; i < arFoto.length; i++){
-            arFoto[i] = a.getDrawable(i);
+            int id = a.getResourceId(i, 0);
 
+            arFoto[i] = ContentResolver.SCHEME_ANDROID_RESOURCE+"://"
+                        +resources.getResourcePackageName(id)+'/'
+                        +resources.getResourceTypeName(id)+'/'
+                    +resources.getResourceEntryName(id);
+            a.recycle();
+
+            for (int i=0;i< arJudul.length; i++)
+            {
+                mList.add(new Hotel(arJudul[i], arDeskripsi[i],
+                        arDetail[i], arLokasi[i], arFoto[i]));
+
+            }
+            mAdapter.notifyDataSetChanged();
+        }
         }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,7 +99,11 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+    @Override
+    protected void onActivityResult(int requestCode, resultCode, data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == )
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -80,4 +118,13 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public void doClick(int pos) {
+        Intent intent= new Intent(this, DetailActivity.class);
+        intent.putExtra(HOTEL,mList.get(pos));
+    }
+
 }
+
